8d3ffce1949e5fdc1290733d5cbeb182df9d725b
Geralt07 <shubhamsarang21@gmail.com>
2024-02-16 21:15:21 +0530

    updated

--- JobCRUD/src/main/java/com/example/jobCRUD/JobCRUD/JobCrudApplication.java
+++ JobCRUD/src/main/java/com/example/jobCRUD/JobCRUD/JobCrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobCrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JobCrudApplication.class, args);
 	}
 
